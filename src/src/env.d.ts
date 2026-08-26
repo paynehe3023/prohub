d33a9a0c21e14c66f50a90524ca17cb3ec5cb4eb
paynehe3023 <paynehe3023@gmail.com>
2026-08-26 16:41:04 +0800

    202608261641update

---
+++ src/src/env.d.ts
@@ -0,0 +1,36 @@
+declare module '@tabler/icons-vue' {
+  import type { DefineComponent } from 'vue'
+
+  type IconComponent = DefineComponent<Record<string, unknown>, Record<string, unknown>, unknown>
+
+  export const IconDownload: IconComponent
+  export const IconPhoto: IconComponent
+  export const IconBox: IconComponent
+  export const IconSun: IconComponent
+  export const IconMoon: IconComponent
+  export const IconChevronRight: IconComponent
+  export const IconSearch: IconComponent
+  export const IconLoader2: IconComponent
+  export const IconVideo: IconComponent
+  export const IconCopy: IconComponent
+  export const IconCheck: IconComponent
+  export const IconLink: IconComponent
+  export const IconUpload: IconComponent
+  export const IconScissors: IconComponent
+  export const IconFileText: IconComponent
+  export const IconPalette: IconComponent
+  export const IconArrowsExchange: IconComponent
+  export const IconCamera: IconComponent
+  export const IconNetwork: IconComponent
+  export const IconClipboardText: IconComponent
+  export const IconAlertTriangle: IconComponent
+  export const IconCoffee: IconComponent
+  export const IconTools: IconComponent
+  export const IconArrowLeft: IconComponent
+  export const IconSparkles: IconComponent
+  export const IconBrandGithub: IconComponent
+  export const IconMoodEmpty: IconComponent
+  export const IconShieldCheck: IconComponent
+  export const IconBolt: IconComponent
+  export const IconDeviceMobile: IconComponent
+}
